c89542f544df2fcb81f7ed3deb4475adcc78b164
srg72 <vic.s@i.ua>
2023-02-24 11:32:55 +0200

    task-6

---
+++ js/task-06.js
@@ -0,0 +1,22 @@
+//Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+
+// Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', onBlur);
+
+function onBlur() {
+  if (!inputEl.value) {
+    inputEl.classList.remove('invalid');
+    inputEl.classList.remove('valid');
+  } else if (inputEl.value.length === +inputEl.dataset.length) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
+  }
+}
